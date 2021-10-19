5da751cf866dcbb66ed95231865df6a919655c9c
krishpranav <krisna.pranav@gmail.com>
2021-10-19 17:32:24 +0530

    api: encoders h264 encoder

--- api/encoders/h264_encoder.go
+++ api/encoders/h264_encoder.go
@@ -13,6 +13,32 @@ type H264Encoder struct {
 	realSize image.Point
 }
 
+func newH264Encoder(size image.Point, frameRate int) (Encoder, error) {
+	buffer := bytes.NewBuffer(make([]byte, 0))
+	realSize, err := findBestSizeForH264Profile(h264SupportedProfile, size)
+	if err != nil {
+		return nil, err
+	}
+	opts := x264.Options{
+		Width:     realSize.X,
+		Height:    realSize.Y,
+		FrameRate: frameRate,
+		Tune:      "zerolatency",
+		Preset:    "veryfast",
+		Profile:   "baseline",
+		LogLevel:  x264.LogWarning,
+	}
+	encoder, err := x264.NewEncoder(buffer, &opts)
+	if err != nil {
+		return nil, err
+	}
+	return &H264Encoder{
+		buffer:   buffer,
+		encoder:  encoder,
+		realSize: realSize,
+	}, nil
+}
+
 const h264SupportedProfile = "3.1"
 
 func (e *H264Encoder) VideoSize() (image.Point, error) {
