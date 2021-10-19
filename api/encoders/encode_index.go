3bcce43f3702de92f2a6069a54c80b965fa7b678
krishpranav <krisna.pranav@gmail.com>
2021-10-19 17:15:03 +0530

    api: encoders
    encode_index

--- api/encoders/encode_index.go
+++ api/encoders/encode_index.go
@@ -1,6 +1,9 @@
 package encoders
 
-import "image"
+import (
+	"fmt"
+	"image"
+)
 
 type encoderFactory = func(size image.Point, frameRate int) (Encoder, error)
 
@@ -11,6 +14,14 @@ type EncoderService struct {
 
 func NewEncoderService() Service {
 	return &EncoderService{}
+}
+
+func (*EncoderService) NewEncoder(codec VideoCodec, size image.Point, frameRate int) (Encoder, error) {
+	factory, found := registeredEncoders[codec]
+	if !found {
+		return nil, fmt.Errorf("Codec not supported")
+	}
+	return factory(size, frameRate)
 }
 
 func (*EncoderService) Supports(codec VideoCodec) bool {
